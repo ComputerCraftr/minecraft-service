29cb3388122c91cc7e3702163d53ec2b5fc01bf7
John Studnicka <contact@zentec.dev>
2024-05-30 01:10:34 -0700

    Create minecraft_config.sh

---
+++ minecraft_config.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Configuration variables for Minecraft server setup
+MINECRAFT_USER="minecraft"
+MINECRAFT_GROUP="minecraft"
+MINECRAFT_DIR="/var/minecraft_server"
+MINECRAFT_JAR="server.jar"
+SERVICE_SH="/usr/local/bin/minecraft_service.sh"
+MONITOR_SCRIPT="/usr/local/bin/minecraft_monitor.sh"
+RESTART_SCRIPT="/usr/local/bin/minecraft_restart.sh"
+PID_FILE="$MINECRAFT_DIR/minecraft_server.pid"
+MEMORY_ALLOCATION="2G"
+INITIAL_MEMORY="256M"
+TMUX_SOCKET="minecraft_socket"
+TMUX_SESSION="minecraft_session"
